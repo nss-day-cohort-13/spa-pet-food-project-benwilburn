1b83d187b2ed435c4b4933e6a6bf98dc7c151d52
Benjamin Wilburn <bwilburnstrength@gmail.com>
2016-05-02 15:25:54 -0500

    got the main javascript funtionality working

--- food.js
+++ food.js
@@ -1,25 +1,36 @@
 var productPage = document.getElementById("products");
-var newRequest = new XMLHttpRequest() /*
-	Description:
-	XMLHttpRequest is an API that provides client functionality for transferring data between a client and a server. It provides an easy way to retrieve data from a URL without having to do a full page refresh. This enables a Web page to update just a part of the page without disrupting what the user is doing. XMLHttpRequest is used heavily in AJAX programming.
-
-	XMLHttpRequest was originally designed by Microsoft and adopted by Mozilla, Apple, and Google. It's now being standardized at the WHATWG. Despite its name, XMLHttpRequest can be used to retrieve any type of data, not just XML, and it supports protocols other than HTTP (including file and ftp).
-
-	Syntax:
-	var myRequest = new XMLHttpRequest();
-*/
-
 
 newRequest.open("GET", "dogFood.json", true);
 newRequest.send();
 
-function init () {
-  var data = JSON.parse(this.responseText);
-  var dogFoodArray = data.dog_brands;
-  for (var i = 0; i < dogFoodArray.length; i++) {
-  	productPage.innerHTML += "<p>" + dogFoodArray[i].name + "</p>"
+function buildDom(foodArray) {
+	console.log(foodArray);
+
+  for (var i = 0; i < foodArray.length; i++) {
+  	// console.log(dogFoodArray);
+  	// console.log(dogFoodArray[i]);
+  	productPage.innerHTML += "<h2>" + foodArray[i].name + "</h2>";
+ 	var arrayType = foodArray[i].types;
+
+    for (var j = 0; j < arrayType.length; j++) {
+	  // console.log(arrayType[j].type);
+	  productPage.innerHTML += "<h4>Type: " + arrayType[j].type + "</h4>";
+	  var arrayVolumes = arrayType[j].volumes;
+
+	  for (var a = 0; a < arrayVolumes.length; a++) {
+	  productPage.innerHTML += "<p> size: " + arrayVolumes[a].name + "</p>" + "<p> price: " + arrayVolumes[a].price + "</p>";
+  		}
+
+  	}
   }
 }
 
-newRequest.addEventListener("load", init);
+function initDog () {
+  var data = JSON.parse(this.responseText);
+  var dogFoodArray = data.dog_brands;
+  buildDom(dogFoodArray);
+}
+
+newRequest.addEventListener("load", initDog);
+newRequest2.addEventListener("load", initCat);
 
